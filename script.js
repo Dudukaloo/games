81b7ee1652f42b0c88c1e23201046eeb78e5b319
Dudukaloo <dudukalu88@gmail.com>
2026-09-22 12:13:56 -0300

    safa

---
+++ script.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+// x, y - Posicionar o objeto 0,0
+// w, h - Definir o tamanho do personagem
+// vx - Define velocidade horizontal
+
+const player = {x: 40, y: 160, w: 32, h: 32, vx: 120}
+
+let last = 0; //Marca a posição do quadro anterior.
+
+function update(dt){
+    player.x += player.vx * dt;
+    //Bateu na parede esquerda ou direita? Inverte o sinal do vx
+    if (player.x < 0 || player.x + player.w > canvas.width){
+        player.vx *= -1;
+    }
+}
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = "#4ade80";
+    ctx.fillRect(player.x, player.y, player.h, player.w);
+
+    // ctx.fillStyle = "#fff";
+    // ctx.fillRect(player.x, player.y, player.w, player.h);
+
+    ctx.fillText("O DeltaTime - dt independente da taxa de quadros", 12, 20);
+}
+
+function loop(ts){
+    if(!last) last = ts;
+
+    const dt = Math.min(0.05, (ts - last)/1000); // ms - segundo
+    last = ts;
+    update(dt);
+    draw();
+    requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);  //Executar o primeiro disparo
