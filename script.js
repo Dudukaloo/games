808ec2552e93dad3ee64b074a175fec0d9f3f37f
Dudukaloo <dudukalu88@gmail.com>
2026-09-24 09:04:28 -0300

    termei a atividade de fazer a bola se locomover na vertical

--- script.js
+++ script.js
@@ -5,22 +5,36 @@ const ctx = canvas.getContext("2d");
 // w, h - Definir o tamanho do personagem
 // vx - Define velocidade horizontal
 
-const player = {x: 40, y: 160, w: 32, h: 32, vx: 120}
+const player = {x: 40, y: 160, w: 32, h: 32, vx: 120, vy:120}
 
 let last = 0; //Marca a posição do quadro anterior.
 
-function update(dt){
+function update(dt){// eu uso dt porque ele garante que a bolinha tenha a mesma velocidade independente do FPS do monitor,
+// assim o movimento é medido em pixels por segundo e não em pixels por quadro,
+// então a bola se move da mesma velocidade em qualquer FPS e em qualquer monitor.
     player.x += player.vx * dt;
+    player.y += player.vy * dt;
     //Bateu na parede esquerda ou direita? Inverte o sinal do vx
     if (player.x < 0 || player.x + player.w > canvas.width){
         player.vx *= -1;
+    }
+    if(player.y < 0 || player.y + player.h > canvas.height){
+        player.vy *= -1 
     }
 }
 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.fillStyle = "#4ade80";
-    ctx.fillRect(player.x, player.y, player.h, player.w);
+    ctx.fillStyle = "#0000FF";
+    const r = player.w / 2;
+    const cx = player.x + r;
+    const cy = player.y + player.h / 2;
+
+    ctx.beginPath();
+    ctx.arc(cx, cy, r, 0, Math.PI * 2);
+    ctx.fill();
+    // ctx.fillStyle = "#4ade80";
+    // ctx.fillRect(player.x, player.y, player.h, player.w);
 
     // ctx.fillStyle = "#fff";
     // ctx.fillRect(player.x, player.y, player.w, player.h);
